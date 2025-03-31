b47ea4de96c26594212f1542c3f24d01d6816d32
Daniel Son <danieljson15@gmail.com>
2025-03-31 17:40:37 -0400

    deployment config

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    // If you're not using a custom domain, uncomment and update:
+    // basePath: '/personal-website',
+    images: {
+      unoptimized: true,
+    },
+  }
+  
+  export default nextConfig
